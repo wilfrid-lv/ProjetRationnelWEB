d0ede6e12f18c7b4b165edd50d3334d8cb442547
Wilfrid Le Valégant <2131345@cegeprdl.ca>
2022-01-28 16:52:50 +0000

    Ajoutez des fichiers projet.

---
+++ ProjetRationnelWEB/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetRationnelWEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
